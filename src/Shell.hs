b830ef7a6f3e4583281e796fb20e0c60b8d09529
Marta Nowakowska <me.nowakowska@student.uw.edu.pl>
2020-04-11 16:53:07 +0200

    Main loop for waiting for events and handling them.

--- src/Shell.hs
+++ src/Shell.hs
@@ -1,4 +1,26 @@
 module Shell where
 
+import Control.Concurrent
+import Control.Concurrent.Async
+import Data.List
+
+type ShellM = IO
+type EventResult = Char
+
+handleEvent :: EventResult -> IO ([Async EventResult])
+handleEvent e = undefined
+
+
+eventsManager :: [Async EventResult] -> IO ()
+eventsManager [] = return ()
+eventsManager events = do
+  (event, res) <- waitAny events
+  newEvents <- handleEvent res
+  eventsManager (delete event (events ++ newEvents))
+
+
 execShell :: IO ()
-execShell = putStrLn "hsh not implemented yet"
+execShell = do
+  char <- async getChar
+  eventsManager ([char])
+  execShell
