c43492774775c14dddedf5de8c9775a47d6a1647
Jacek Olczyk <jacek.olczyk98@gmail.com>
2020-04-14 10:02:38 +0200

    Create test console app that prints its output doubled.

--- src/Shell.hs
+++ src/Shell.hs
@@ -1,5 +1,6 @@
 module Shell where
 
+import System.Exit
 import Control.Monad.IO.Class
 import UnliftIO
 import Data.List
@@ -8,25 +9,34 @@ import System.Console.Haskeline
 type ShellM = IO
 type EventResult = Maybe String
 
-handleEvent :: EventResult -> IO [Async EventResult]
-handleEvent e = undefined
+type EventList = [Async EventResult]
 
+data Action = APrint String | AExit (Maybe Int) deriving (Show, Eq)
 
-eventsManager :: [Async EventResult] -> IO ()
+handleEvent :: EventResult -> [Action]
+handleEvent Nothing = [AExit Nothing]
+handleEvent (Just s) = [APrint $ s ++ s]
+
+execAction :: MonadIO m => Action -> m ()
+execAction = liftIO . go
+  where
+    go (APrint s) = putStrLn s
+    go (AExit Nothing) = exitSuccess
+    go (AExit (Just code)) = exitWith (ExitFailure code)
+
+eventsManager :: EventList -> InputT IO ()
 eventsManager [] = return ()
 eventsManager events = do
   (event, res) <- waitAny events
-  newEvents <- handleEvent res
-  eventsManager (delete event (events ++ newEvents))
+  mapM_ execAction $ handleEvent res
+  eventsManager $ delete event events
 
 
 execShell :: IO ()
-execShell = do
-  runInputT defaultSettings loop
+execShell = runInputT defaultSettings loop
   where
     loop :: InputT IO ()
     loop = do
       input <- async $ getInputLine "hashell$ "
-      liftIO $ eventsManager [input]
-      return ()
-      --eventsManager ([input])
+      eventsManager [input]
+      loop
